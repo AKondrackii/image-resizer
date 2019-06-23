207e1088575ce0943a1957df60a05feb41d6900f
Artem <art.kondratscky@yandex.ru>
2019-06-23 17:09:47 +0300

    added some code

--- main.py
+++ main.py
@@ -1,5 +1,40 @@
+import os
+from PIL import Image
+
 def main():
-    print("Hello world!")
+    params = get_params()
+    print(params)
+    resize_image(params[0], params[1])
+
+def get_params():
+    where_dir = input("Расположение изображения/изображение: \t")
+
+    while True:
+        try:
+            width = int(input("Введите ширину изображения\t"))
+        except ValueError:
+            print("Это не число, введите заново")
+        else:
+            break
+
+    while True:
+        try:
+            height = int(input("Введите высоту изображения\t"))
+        except ValueError:
+            print("Это не число, введите заново")
+        else:
+            break
+    
+
+    return [ where_dir, [ width, height ] ]
+
+def resize_image(image_path, size):
+    # original_image = Image.open(image_path)
+
+    width = size[0]
+    height = size[1]
+
+    print("Width = {width}\tHeight = {height}".format(width = width, height = height))
 
 if __name__ == "__main__":
     main()
