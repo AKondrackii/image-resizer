64206907d17bf7bd94a6a7b397bc99b268d34539
Artem <art.kondratscky@yandex.ru>
2019-06-23 18:00:49 +0300

    save

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import os, sys
+# -*- coding:utf -8 -*-
+import os
 from PIL import Image
 
 def main():
     params = get_params()
     print(params)
-    resize_image(params[0], params[1])
+    # resize_image(params[0], params[1])
+    scale_image(params[0], params[1][0], params[1][1])
 
 def get_params():
     # project_dir = input("Расположение изображения/изображение: \t")
@@ -40,7 +42,31 @@ def resize_image(image_path, size):
     print("Width = {width}\tHeight = {height}".format(width = width, height = height))
 
     resized_image.show()
-    resized_image.save(image_path + '\mountains_resized.jpg')
+    resized_image.save(image_path + "\mountains_resized.jpg")
+
+def scale_image(image_path, width = None, height = None):
+    original_image = Image.open(image_path + "\mountains.jpg")
+    w, h = original_image.size
+    print('The original image size is {wide} wide x {height} '
+          'high'.format(wide=w, height=h))
+ 
+    if width and height:
+        max_size = (width, height)
+    elif width:
+        max_size = (width, h)
+    elif height:
+        max_size = (w, height)
+    else:
+        # No width or height specified
+        raise RuntimeError('Width or height required!')
+ 
+    original_image.thumbnail(max_size, Image.ANTIALIAS)
+    original_image.save(image_path + "\mountains_resized.jpg")
+ 
+    scaled_image = Image.open(image_path + "\mountains_resized.jpg")
+    width, height = scaled_image.size
+    print('The scaled image size is {wide} wide x {height} '
+          'high'.format(wide=width, height=height))
 
 if __name__ == "__main__":
     main()
