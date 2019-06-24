58a64b732bf7e5fa4ea8dbe6b8a9b09ee618e2a9
Artem <art.kondratscky@yandex.ru>
2019-06-24 14:11:56 +0300

    first step

--- main.py
+++ main.py
@@ -1,47 +1,23 @@
 # -*- coding:utf -8 -*-
 import os, glob
-from PIL import Image
+# from PIL import Image
+import tkinter as tk
+from tkinter import ttk
 
-def main():
-    params = get_params()
-    for picture in glob.glob(os.getcwd() + "\original\*.*"):
-        resize_image(picture, params)
+class Main(tk.Frame):
+    def __init__(self, root):
+        super().__init__(root)
+        self.init_main()
 
-def get_params():
-    # project_dir = input("Расположение изображения/изображение: \t")
+    def init_main(self):
+        pass
 
-    while True:
-        try:
-            width = int(input("Введите ширину изображения\t"))
-        except ValueError:
-            print("Это не число, введите заново")
-        else:
-            break
-
-    while True:
-        try:
-            height = int(input("Введите высоту изображения\t"))
-        except ValueError:
-            print("Это не число, введите заново")
-        else:
-            break
-    
-    return ( width, height )
-
-def resize_image(image_path, size):
-    original_image = Image.open(image_path)
-    original_image.thumbnail(size, Image.ANTIALIAS)
-    original_image.save(image_path.replace("\\original", "\\resized"))
 
 if __name__ == "__main__":
-    try:
-        os.mkdir("original")
-    except:
-        print("Папка original уже создана")
-    
-    try:
-        os.mkdir("resized")
-    except:
-         print("Папка resized уже создана")
-    
-    main()
+    root = tk.Tk()
+    app = Main(root)
+    app.pack()
+    root.title("Image Resizer")
+    root.geometry("650x450+300+200")
+    root.resizable(False, False)
+    root.mainloop()
